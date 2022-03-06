777e144a917951417f3128de8d2d18f3cfdf5bb4
IshanBhargava <ishanbhargava@gmail.com>
2022-03-06 20:03:39 +0530

    switched to pycord from discord.py, modified the code structure, commands are in botCommands file
    Signed-off-by: IshanBhargava <ishanbhargava@gmail.com>

--- ApiService.py
+++ ApiService.py
@@ -1,13 +1,7 @@
 import requests
 
 
-def chucknorrisjokes() -> str:
-    url = "https://api.chucknorris.io/jokes/random"
-    response = requests.get(url)
-    return response.json()["value"]
-
-
 def joke() -> str:
-    url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit&type=single"
+    url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
     response = requests.get(url)
     return response.json()["joke"]
